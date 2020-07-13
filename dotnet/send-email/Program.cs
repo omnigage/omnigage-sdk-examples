12c8adea0ead86f2f7b20e7f1f47b8c7739bf110
Micheal Morgan <mail@michealmorgan.com>
2020-07-13 13:07:14 -0500

    Add example of sending individual email

---
+++ dotnet/send-email/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Omnigage;
+using Omnigage.Resource;
+using Omnigage.Runtime;
+
+namespace send_sms
+{
+    public class Program
+    {
+        /// <summary>
+        /// To run this application, the following is required:
+        ///
+        /// - API token key/secret from Account -> Developer -> API Tokens
+        /// - A verified Phone Number UUID from Account -> Email -> Email IDs -> Edit (in the URI)
+        /// - Fill in variables to run example
+        /// </summary>
+        /// <param name="args"></param>
+        async static Task Main(string[] args)
+        {
+            var tokenKey = "";
+            var tokenSecret = "";
+
+            // Set the Email ID (e.g., NbXW9TCHax9zfAeDhaY2bG)
+            var emailId = "";
+
+            // Recipient email address
+            var to = "";
+
+            // Initialize SDK
+            OmnigageClient.Init(tokenKey, tokenSecret);
+
+            // Create email message
+            var message = new EmailMessageResource
+            {
+                Subject = "Ahoy",
+                Body = "Sample body"
+            };
+            await message.Create();
+
+            // Send email
+            var email = new EmailResource
+            {
+                To = to,
+                EmailMessage = message,
+                EmailId = new EmailIdResource
+                {
+                    Id = emailId
+                }
+            };
+            await email.Create();
+        }
+    }
+}
